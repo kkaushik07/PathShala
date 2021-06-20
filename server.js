463c6fd30cf04467dbca958d7f3c7049418af1a0
kkaushik07 <82221159+kkaushik07@users.noreply.github.com>
2021-06-20 18:39:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(passport.session())
 
 
 //mongoDB
-mongoose.connect('mongodb+srv://dbUser:mongod@cluster0.o1hzn.mongodb.net/udemyDB?retryWrites=true&w=majority',{
+mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
